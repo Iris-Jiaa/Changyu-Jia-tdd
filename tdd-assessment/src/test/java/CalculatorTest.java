16f31c516e110bd3280f9aa750a4ca6261c8c00c
Changyu Jia <IrisJia0201@outlook.com>
2025-10-17 01:26:38 +0800

    test4

--- tdd-assessment/src/test/java/CalculatorTest.java
+++ tdd-assessment/src/test/java/CalculatorTest.java
@@ -3,21 +3,25 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 import src.main.java.Calculator;
 public class CalculatorTest {
+    private Calculator c = new Calculator();
     @Test //test 1
     void testAdd(){
-        Calculator c = new Calculator();
         assertEquals(5, c.add(2, 3));
     }
     @Test //test 2
     void testSubtract() { 
-        Calculator c = new Calculator(); 
         assertEquals(5, c.subtract(8, 3)); 
     } 
 
     @Test 
     void testMultiply() { 
-        Calculator c = new Calculator(); 
         assertEquals(24, c.multiply(8, 3)); 
     }
 
+    @Test 
+    void testDivide() { 
+        assertEquals(2, c.divide(6, 3)); 
+    } 
 }
+
+
